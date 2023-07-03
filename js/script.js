ba3f63f3dca89f2a3c0aeb33abb36bce7aba87c7
Augusto <augustoramos120@gmail.com>
2023-07-03 09:12:49 -0300

    Commit do css e html e primeiro play javascript

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Função play musica
+
+  //var audio = document.getElementById('audio');
+  //var playPauseBTN = document.getElementById('playPauseBTN');
+  //var count = 0;
+
+  //function playPause(){
+   // if(count == 0){
+     //   count = 1;
+      //  audio.play();
+   // }else{
+     //   count = 0;
+     //   audio.pause();
+    //}
+  //}
+
+  var audio = new Audio('audio/audio-index/frase-index.mp3');
+      
+  function playAudio() {
+    audio.play();
+  }
+  var count = 0;
+
+  //function playPause(){
+   // if(count == 0){
+     //   count = 1;
+      //  audio.play();
+   // }else{
+     //   count = 0;
+     //   audio.pause();
+
+
+
+
+
+
+
+
